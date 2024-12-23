024ff6884ecb1943e355d243368a8c030ee269e9
Adam Edison <adam.edison@gmail.com>
2024-12-23 12:41:32 -0500

    prevent microphone from picking up phone speaker on speech playback

--- tts.py
+++ tts.py
@@ -18,7 +18,12 @@ def speak(text: str):
 class Actions:
   def tts_speak(text: str) -> None:
     """Speak supplied text"""
+    actions.sound.set_microphone("None")
+
     speak(text)
 
+    pauseSeconds = 3 + (len(text) * 0.1)  
+    actions.sleep(pauseSeconds)
+    actions.sound.set_microphone("BlackHole 2ch")
 
 
